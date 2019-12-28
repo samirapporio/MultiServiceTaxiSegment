a45a82e8ce8b35210ab6a96de80ccee95c0ce7b4
Samir Goel <samir@apporio.com>
2019-12-28 17:53:11 +0530

    map installed in library

--- app/src/main/java/com/apporioinfolabs/apporiomultiservicetaxisegment/MainActivity.java
+++ app/src/main/java/com/apporioinfolabs/apporiomultiservicetaxisegment/MainActivity.java
@@ -2,7 +2,11 @@ package com.apporioinfolabs.apporiomultiservicetaxisegment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.apporioinfolabs.taxisegment.MainActivityTaxi;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.taxi_segment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivityTaxi.class));
+            }
+        });
     }
 }
